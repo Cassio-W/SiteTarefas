b43c44ad9c06ba457ddbf83cbee3f5bd7dbaedbf
Cassio <cassioweissheimer2006@gmail.com>
2025-08-20 17:19:05 -0300

    Change BrowserRouter to HashRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import MainPage from "./pages/MainPage";
 
 function App() {
   return(
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainPage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/register" element={<RegisterPage/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
