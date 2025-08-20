f9371da2ad917b7aa9132f3fd1a01482a5fe5fd3
Cassio <cassioweissheimer2006@gmail.com>
2025-08-20 17:46:19 -0300

    Removed detailed Cors authorization

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,8 +10,6 @@ async function bootstrap() {
       'https://site-tarefas-iwkl.onrender.com'
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 
   await app.listen(process.env.PORT ?? 3000);
