a9c1dcb8ec1e799c45adb65074e1a9b44133f104
Cassio <cassioweissheimer2006@gmail.com>
2025-08-27 19:06:54 -0300

    Pages default styles change and Card.tsx size adjustment

--- frontend/src/components/core/Card.tsx
+++ frontend/src/components/core/Card.tsx
@@ -6,14 +6,14 @@ type CardProps = {
 
 function Card({children, extraClasses = "", isCenter = true}: CardProps) {
   if (!isCenter) return (
-    <div className={"bg-gray-800 p-6 rounded-xl shadow-md max-w-md border-blue-800 border-5 " + extraClasses}>
+    <div className={"bg-gray-800 p-6 rounded-xl shadow-md w-[380px] border-blue-800 border-5 " + extraClasses}>
       {children}
     </div>
   )
   
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className={"bg-gray-800 p-6 rounded-xl shadow-md max-w-md border-blue-800 border-5 z-10 " + extraClasses}>
+      <div className={"bg-gray-800 p-6 rounded-xl shadow-md w-[380px] border-blue-800 border-5 z-10 " + extraClasses}>
         {children}
       </div>
     </div>
